2296754fa14d2ef687f730a1463a72681e12e9f9
Patryk Golebiowski <pgolebiowski@users.noreply.github.com>
2015-11-27 01:41:09 +0100

    Solution structure setup.
    Created three empty projects.

---
+++ src/Onism.Cldr.TestConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Onism.Cldr.TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cldr.NET Test Console");
+
+            Console.WriteLine("Doing stuff...");
+
+
+            Console.WriteLine();
+            Console.WriteLine("Finished");
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
