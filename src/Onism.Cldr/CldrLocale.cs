aabf224b2d9ceff4862a144e3907032352a47700
pgolebiowski <patryk.golebiowski@uj.edu.pl>
2015-11-30 03:15:59 +0100

    Full support for CldrJson.
    Implemented and tested CldrJson - an in-memory representation of a
    single CLDR JSON file. It facilitates further process of parsing CLDR
    data. This class consists mostly of validation.

---
+++ src/Onism.Cldr/CldrLocale.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Onism.Cldr
+{
+    public class CldrLocale
+    {
+        public string LocaleCode { get; set; }
+
+        [JsonProperty(PropertyName = "language")]
+        public string Language { get; set; }
+
+        [JsonProperty(PropertyName = "territory")]
+        public string Territory { get; set; }
+
+        [JsonProperty(PropertyName = "script")]
+        public string Script { get; set; }
+
+        [JsonProperty(PropertyName = "variant")]
+        public string Variant { get; set; }
+    }
+}
---
+++ src/Onism.Cldr/Extensions/JTokenExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace Onism.Cldr.Extensions
+{
+    public static class JTokenExtensions
+    {
+        public static HashSet<JTokenType> GetAllTypes(this JToken token)
+        {
+            var types = new HashSet<JTokenType>();
+            types.Add(token.Type);
+
+            foreach (var child in token.Children())
+                foreach (var type in child.GetAllTypes())
+                    types.Add(type);
+
+            return types;
+        }
+
+        public static IEnumerable<JToken> FindLeaves(this JToken token)
+        {
+            var children = token.Children();
+
+            if (children.IsNotEmpty())
+                return children.SelectMany(x => x.FindLeaves());
+            else
+                return token;
+        }
+    }
+}
