cfac70ce0b897e0887cb8cfbf757f437fc8de954
Patryk Golebiowski <pgolebiowski@users.noreply.github.com>
2015-11-30 18:27:18 +0100

    CldrLocale support added.
    Better structure, additional properties, tests, and equality checking.

---
+++ src/Onism.Cldr.Test/CldrLocaleTest.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace Onism.Cldr.Test
+{
+    [TestFixture]
+    public class CldrLocaleTest
+    {
+        private static CldrLocale Arrange(string language, string territory, string script, string variant)
+        {
+            return new CldrLocale()
+            {
+                Language = language,
+                Territory = territory,
+                Script = script,
+                Variant = variant
+            };
+        }
+
+        private static CldrLocale enGB => Arrange("en", "GB", null, null);
+
+        [Test]
+        public void Equals_InstanceAndNull_False()
+        {
+            var l = enGB;
+
+            Equals(l, null).Should().BeFalse();
+            Equals(null, l).Should().BeFalse();
+
+            (l == null).Should().BeFalse();
+            (null == l).Should().BeFalse();
+        }
+
+        [Test]
+        public void Equals_SameValues_True()
+        {
+            var l1 = enGB;
+            var l2 = enGB;
+
+            l1.Should().Be(l2);
+            l2.Should().Be(l1);
+
+            (l1 == l2).Should().BeTrue();
+            (l2 == l1).Should().BeTrue();
+        }
+
+        [Test]
+        public void Equals_SameReferences_True()
+        {
+            var l1 = enGB;
+            var l2 = l1;
+
+            l1.Should().Be(l2);
+            l2.Should().Be(l1);
+
+            (l1 == l2).Should().BeTrue();
+            (l2 == l1).Should().BeTrue();
+
+            Action a = (() => enGB.GetHashCode());
+            a.ShouldNotThrow();
+        }
+
+        [Test]
+        public void Equals_DifferentTypes_False()
+        {
+            object l1 = enGB;
+            object l2 = "enGB";
+
+            l1.Should().NotBe(l2);
+            l2.Should().NotBe(l1);
+
+            (l1 == l2).Should().BeFalse();
+            (l2 == l1).Should().BeFalse();
+        }
+
+        [Test]
+        public void Equals_SameButBoxed_True()
+        {
+            object l1 = enGB;
+            object l2 = enGB;
+
+            l1.Should().Be(l2);
+            l2.Should().Be(l1);
+        }
+
+        [Test]
+        public void Equals_SlightlyDifferent_False()
+        {
+            var l1 = Arrange("a", "b", "c", "d");
+            var l2 = Arrange("a", "b", "c", "e");
+
+            l1.Should().NotBe(l2);
+            l2.Variant = "d";
+            l1.Should().Be(l2);
+
+            l1.Script = "f";
+            l1.Should().NotBe(l2);
+            l2.Script = "f";
+            l1.Should().Be(l2);
+
+            l1.Territory = "g";
+            l1.Should().NotBe(l2);
+            l2.Territory = "g";
+            l2.Should().Be(l2);
+        }
+    }
+}
--- src/Onism.Cldr/CldrLocale.cs
+++ src/Onism.Cldr/CldrLocale.cs
@@ -7,20 +7,106 @@ using Newtonsoft.Json;
 
 namespace Onism.Cldr
 {
+    /// <summary>
+    /// Unicode locale identifier used in CLDR JSON data.
+    /// Consists of language, territory, script, and variant subtags.
+    /// </summary>
+    /// <remarks>
+    /// The locale identifier used in CLDR JSON data is in fact a language identifier,
+    /// since no locale extensions are being used. However, this might be changed in future CLDR releases.
+    /// See the Core Specification (http://cldr.unicode.org/core-spec) for definitions.
+    /// </remarks>
     public class CldrLocale
     {
-        public string LocaleCode { get; set; }
-
+        /// <summary>
+        /// Unicode language subtag (also known as a Unicode base language code).
+        /// </summary>
         [JsonProperty(PropertyName = "language")]
         public string Language { get; set; }
 
+        /// <summary>
+        /// Unicode region subtag (also known as a Unicode region code, or a Unicode territory code).
+        /// </summary>
         [JsonProperty(PropertyName = "territory")]
         public string Territory { get; set; }
 
+        /// <summary>
+        /// Unicode script subtag (also known as a Unicode script code).
+        /// </summary>
         [JsonProperty(PropertyName = "script")]
         public string Script { get; set; }
 
+        /// <summary>
+        /// Unicode variant subtag (also known as a Unicode language variant code)
+        /// </summary>
         [JsonProperty(PropertyName = "variant")]
         public string Variant { get; set; }
+
+        /// <summary>
+        /// A locale that only has a language subtag (and optionally a script subtag)
+        /// is called a language locale.
+        /// </summary>
+        public bool IsLanguageLocale => (Language != null) && (Territory == null) && (Variant == null);
+
+        /// <summary>
+        /// A locale with both language and territory subtag is called
+        /// a territory locale (or country locale).
+        /// </summary>
+        public bool IsTerritoryLocale => (Language != null) && (Territory != null);
+
+        /// <summary>
+        /// Gets the hyphen-separated code of this <see cref="CldrLocale"/>.
+        /// </summary>
+        public string Code => JoinNotNull("-", Language, Territory, Script, Variant);
+
+        private string JoinNotNull(string separator, params string[] values)
+        {
+            return string.Join(separator, values.Where(x => x != null));
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+                return false;
+
+            if (ReferenceEquals(this, obj))
+                return true;
+
+            if (obj.GetType() != typeof(CldrLocale))
+                return false;
+
+            return Equals((CldrLocale)obj);
+        }
+
+        public bool Equals(CldrLocale other)
+        {
+            if (ReferenceEquals(null, other))
+                return false;
+
+            if (ReferenceEquals(this, other))
+                return true;
+
+            return Language == other.Language
+                && Territory == other.Territory
+                && Script == other.Script
+                && Variant == other.Variant;
+        }
+
+        public override int GetHashCode()
+        {
+            return (new string[] { Language, Territory, Script, Variant })
+                .Select(x => x == null ? 0 : x.GetHashCode())
+                .Aggregate((a, b) => a ^ b);
+        }
+
+        public static bool operator ==(CldrLocale left, CldrLocale right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(CldrLocale left, CldrLocale right)
+        {
+            return !Equals(left, right);
+        }
     }
 }
