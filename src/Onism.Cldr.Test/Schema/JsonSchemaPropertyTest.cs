de74290df8471f2db491b4d07ef1af1f190e5ca1
Patryk Golebiowski <pgolebiowski@users.noreply.github.com>
2016-04-14 20:11:38 +0200

    Added JsonSchemaProperty and JsonSchemaValue

---
+++ src/Onism.Cldr.Test/Schema/JsonSchemaPropertyTest.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Onism.Cldr.Schema;
+
+namespace Onism.Cldr.Test.Schema
+{
+    [TestFixture]
+    public class JsonSchemaPropertyTest
+    {
+        [TestCase("text", Result = "text")]
+        [TestCase("~text", Result = "text")]
+        [TestCase("@text", Result = "text")]
+        [TestCase("~@text", Result = "text")]
+        [TestCase("~@爱", Result = "爱")]
+        public string NameShouldBeExtracted(string expression)
+        {
+            return JsonSchemaProperty.Parse(expression).Name;
+        }
+
+        [TestCase("text", Result = false)]
+        [TestCase("~text", Result = true)]
+        [TestCase("@text", Result = false)]
+        [TestCase("~@text", Result = true)]
+        [TestCase("~@爱", Result = true)]
+        public bool OptionalityShouldBeExtracted(string expression)
+        {
+            return JsonSchemaProperty.Parse(expression).IsOptional;
+        }
+
+        [TestCase("text", Result = false)]
+        [TestCase("~text", Result = false)]
+        [TestCase("@text", Result = true)]
+        [TestCase("~@text", Result = true)]
+        [TestCase("~@爱", Result = true)]
+        public bool VariablesShouldBeDetected(string expression)
+        {
+            var result = JsonSchemaProperty.Parse(expression).IsVariable;
+            return result;
+        }
+    }
+}
