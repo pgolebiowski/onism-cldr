6f63b628d03ed7192712004380f42e5ee66fdcd1
Patryk Golebiowski <pgolebiowski@users.noreply.github.com>
2016-04-17 17:25:03 +0200

    Changing the approach
    Dropping the idea of parsing CLDR data using handlers for various types
    of packages. CLDR JSONs will not be downloaded by the application
    anymore - it will be the responsibility of the user.

--- src/Onism.Cldr/CldrData.cs
+++ src/Onism.Cldr/CldrData.cs
@@ -2,11 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using Onism.Cldr.Extensions;
-using Onism.Cldr.Packages;
-using Onism.Cldr.Utils;
 using ProtoBuf;
 
 namespace Onism.Cldr
@@ -17,108 +12,6 @@ namespace Onism.Cldr
     [ProtoContract]
     public class CldrData
     {
-        #region Properties
-
-        /// <summary>
-        /// <see cref="CldrTree"/> object containing localization data.
-        /// </summary>
-        [ProtoMember(1)]
-        public CldrTree Standard { get; private set; }
-
-        /// <summary>
-        /// Serialized JSON object containing supplemental CLDR data, merged in a safe way.
-        /// Use your favourite tool to browse this object.
-        /// </summary>
-        [ProtoMember(2)]
-        public string Supplemental { get; private set; }
-
-        /// <summary>
-        /// Serialized JSON object containing line breaking data from Unicode's ULI project,
-        /// merged in a safe way. Use your favourite tool to browse this object.
-        /// </summary>
-        [ProtoMember(3)]
-        public string Segments { get; private set; }
-
-        #endregion
-
-        #region Creation
-
-        /// <summary>
-        /// Downloads the specified CLDR packages from GitHub and merges them.
-        /// </summary>
-        /// <param name="packages">The packages to be downloaded and merged.</param>
-        public static CldrData Download(params CldrPackage[] packages)
-        {
-            var tempPath = Path.GetTempPath();
-            var packagesDirectory = Path.Combine(tempPath, "cldr-packages");
-            Directory.CreateDirectory(packagesDirectory);
-
-            packages.ForEach(x => x.Download(packagesDirectory));
-            var result = MergePackages(packagesDirectory);
-            Directory.Delete(packagesDirectory, true);
-
-            return result;
-        }
-
-        /// <summary>
-        /// Searches the specified directory, looking for CLDR package files.
-        /// Once found, their data is merged.
-        /// </summary>
-        /// <param name="directoryName">The relative or absolute path to the directory to search.</param>
-        public static CldrData MergePackages(string directoryName)
-        {
-            var packages = Directory.GetFiles(directoryName, $"*{CldrPackage.Extension}");
-            return MergePackages(packages);
-        }
-
-        /// <summary>
-        /// Merges the data found in the specified CLDR package files.
-        /// </summary>
-        /// <param name="paths">The paths to package files to be merged.</param>
-        public static CldrData MergePackages(string[] paths)
-        {
-            // these objects will be modified untill
-            // all packages are parsed
-            var standard = new CldrTree();
-            var supplemental = new JObject();
-            var segments = new JObject();
-
-            // "smart switch" type
-            var @switch = new Dictionary<Type, Action<CldrJson>>
-            {
-                {
-                    typeof(CldrStandardPackage),
-                    json => standard.Add(json)
-                },
-                {
-                    typeof(CldrSupplementalPackage),
-                    json => JsonUtils.SafeMerge(supplemental, json.Data)
-                },
-                {
-                    typeof(CldrSegmentsPackage),
-                    json => JsonUtils.SafeMerge(segments, json.Data)
-                }
-            };
-
-            // by design, package files are in fact collections of CldrJson objects.
-            // thus, it is sufficient to read all package files, extract those CldrJson
-            // objects from them and aggregate their data properly (the switch above)
-            paths
-                .Select(File.ReadAllText)
-                .SelectMany(JsonConvert.DeserializeObject<CldrJson[]>)
-                .ForEach(cldrJson => @switch[cldrJson.Package](cldrJson));
-
-            // all the packages have already been parsed
-            return new CldrData()
-            {
-                Standard = standard,
-                Supplemental = supplemental.ToString(),
-                Segments = segments.ToString()
-            };
-        }
-
-        #endregion
-
         #region Serialization
 
         /// <summary>
