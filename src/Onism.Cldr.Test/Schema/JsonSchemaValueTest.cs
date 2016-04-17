f95aa40827da475d511ee0d0dde346592ffcfee3
Patryk Golebiowski <pgolebiowski@users.noreply.github.com>
2016-04-17 16:44:33 +0200

    Updated packages

--- src/Onism.Cldr.Test/Schema/JsonSchemaValueTest.cs
+++ src/Onism.Cldr.Test/Schema/JsonSchemaValueTest.cs
@@ -12,24 +12,24 @@ namespace Onism.Cldr.Test.Schema
     [TestFixture]
     public class JsonSchemaValueTest
     {
-        [TestCase("type @text", Result = "type")]
-        [TestCase("爱 @text", Result = "爱")]
+        [TestCase("type @text", ExpectedResult = "type")]
+        [TestCase("爱 @text", ExpectedResult = "爱")]
         public string TypeShouldBeExtracted(string expression)
         {
             return JsonSchemaValue.Parse(expression).Type;
         }
 
-        [TestCase("type text", Result = "text")]
-        [TestCase("type @text", Result = "text")]
-        [TestCase("type @爱", Result = "爱")]
+        [TestCase("type text", ExpectedResult = "text")]
+        [TestCase("type @text", ExpectedResult = "text")]
+        [TestCase("type @爱", ExpectedResult = "爱")]
         public string NameShouldBeExtracted(string expression)
         {
             return JsonSchemaValue.Parse(expression).Name;
         }
 
-        [TestCase("type text", Result = false)]
-        [TestCase("type @text", Result = true)]
-        [TestCase("type @爱", Result = true)]
+        [TestCase("type text", ExpectedResult = false)]
+        [TestCase("type @text", ExpectedResult = true)]
+        [TestCase("type @爱", ExpectedResult = true)]
         public bool VariablesShouldBeDetected(string expression)
         {
             return JsonSchemaValue.Parse(expression).IsVariable;
