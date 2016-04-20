1b23f1220d063ca7d6544892c455c270a2ad7918
Patryk Golebiowski <pgolebiowski@users.noreply.github.com>
2016-04-21 00:29:19 +0200

    Added FileFinder and VersionConsistencyAssurer

---
+++ src/Onism.Cldr/CldrDataBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Onism.Cldr
+{
+    public class CldrDataBuilder
+    {
+        private readonly CldrJsonFileFinder fileFinder;
+        private readonly CldrVersionConsistencyAssurer versionConsistencyAssurer;
+
+        public CldrDataBuilder()
+        {
+            this.fileFinder = new CldrJsonFileFinder();
+            this.versionConsistencyAssurer = new CldrVersionConsistencyAssurer();
+        }
+
+        public void Build(string directory)
+        {
+            foreach (var file in fileFinder.FindFiles(directory))
+            {
+                // versionConsistencyAssurer.AssureVersionIsConsistent();
+            }
+        }
+    }
+}
---
+++ src/Onism.Cldr/CldrJsonFileFinder.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Onism.Cldr
+{
+    public class CldrJsonFileFinder
+    {
+        public IEnumerable<string> FindFiles(string directory)
+        {
+            return Directory
+                .EnumerateFiles(directory, "*.json", SearchOption.AllDirectories)
+                .Where(IsCldrJsonFile);
+        }
+
+        private static bool IsCldrJsonFile(string path)
+        {
+            var fileName = Path.GetFileNameWithoutExtension(path);
+            return FilesToIgnore.Contains(fileName) == false;
+        }
+
+        private static readonly HashSet<string> FilesToIgnore = new HashSet<string>
+        {
+            "bower",
+            "package"
+        };
+    }
+}
